89e7355a586a242e2ef801e8ab4ab0c9223ef59b
SebastianSamanieg <sebastian_samaniego@outlook.com>
2023-09-21 11:17:28 -0500

    Se modifica codigo para que traiga el texto completo

--- main.py
+++ main.py
@@ -13,13 +13,12 @@ def root():
 
 @app.post("/indices-invertidos")
 def indices_invertidos(palabra: dict):
-    mook.my_documents
     cache = {}
     for index, documento in enumerate(mook.my_documents):
         words = documento.lower().split()
         for word in words:
             if word in cache:
-                cache[word].append(index)
+                cache[word].append(mook.my_documents[index])
             else:
-                cache[word] = [index]
+                cache[word] = [mook.my_documents[index]]
     return cache.get(palabra["palabra"], "No se encontro")
